28b9f592aee749115464f9b63fb0293e09935469
Timothy Reen <reentim@gmail.com>
2016-07-12 20:34:01 +1000

    Seed database with dummy data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+100.times { Lesson.create!(name: Faker::Educator.course) }
+3.times { Teacher.create!(name: "Mr. #{Faker::Name.last_name}") }
+9.times { ClassGroup.create!(teacher: Teacher.all.to_a.sample) }
+
+180.times { Student.create!(name: Faker::Name.name)}
+
+Student.all.each do |student|
+  class_group = ClassGroup.all.to_a.sample
+
+  Random.rand(1..99).times do |n|
+    student.enrollments.new(
+      lesson: Lesson.find(n + 1),
+      class_group: class_group,
+      parts_completed: 3,
+    ).save(validate: false)
+  end
+
+  student.enrollments.new(
+    lesson: Lesson.find(Enrollment.last.lesson.id + 1),
+    class_group: class_group,
+    parts_completed: Random.rand(3),
+  ).save(validate: false)
+end
