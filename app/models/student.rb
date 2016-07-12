089712a04f475c02ca45a470dbb68b53acd8520d
Timothy Reen <reentim@gmail.com>
2016-07-12 20:34:32 +1000

    Make teacher student progress report more concise.

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,8 @@ class Student < ApplicationRecord
   has_many :lessons, through: :enrollments
   has_many :class_groups, through: :enrollments
   has_many :teachers, through: :class_groups
+
+  def last_completed_lesson
+    lessons.where()
+  end
 end
